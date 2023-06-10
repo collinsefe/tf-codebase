8a56d51439fea57c4acc6a510f24be3b52c4454b
Collins Orighose <collinsefe@yahoo.com>
2023-06-10 15:09:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "collinsorighose-tf"
-    //bucket = var.bucket
+    //bucket = "collinsorighose-tf"
+    bucket = var.bucket
     key    = var.key
     //key    = "dev/terraform.tfstate"
     region = "eu-west-2"
